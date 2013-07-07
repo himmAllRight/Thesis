4bd62ae1eb38395e4f39c741474650f536b18dd1
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2013-07-07 17:38:52 -0400

    added variables to graph creating for greater controll

--- make_model.r
+++ make_model.r
@@ -9,8 +9,11 @@ library(igraph)
 ############### Defined Functions ###############
 #################################################
 
-makeSWPNetwork <- function(dim,nodeCount){
-g <- watts.strogatz.game(dim,nodeCount,2,.5, loops = FALSE, multiple = FALSE)
+makeSWPNetwork <- function(dim,size,nei,p){
+g <- watts.strogatz.game(dim,size,nei,p, loops = FALSE, multiple = FALSE)
+
+# print(hub.score(g))
+
 return(g)
 }
 
@@ -20,12 +23,14 @@ return(g)
 #################################################
 
 ## Model Parameters
-dim       = 1
-nodeCount = 25
+dim       = 1	# Interger Constant, the demension of the starting lattice
+size      = 2 	# The size of the lattice along each dimension
+nei       = 2   # the neighborhood within which the verticies of the lattice will be connected
+p         = .5  # the rewiring probabillity
+
 
 ## Execute
-swpGraph = makeSWPNetwork(dim,nodeCount)
-
+swpGraph = makeSWPNetwork(dim,size,nei,p)
 ## Plot
 png(file="SWP_plot1.png")
 plot(swpGraph)
