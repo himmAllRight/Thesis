d37fcaac8505d5af1b66e9710774ceded1921529
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2013-08-27 11:22:12 -0400

    added function to generate erdos-renyi random graph
     Changes to be committed:
       (use "git reset HEAD^1 <file>..." to unstage)
    
    	modified:   make_model.r

--- make_model.r
+++ make_model.r
@@ -1,7 +1,7 @@
 ## Ryan Himmelwright
 ## Honors Thesis
 ## Make Model Script
-## 5/31/13
+## 7/7/13
 
 library(igraph)
 
@@ -9,30 +9,51 @@ library(igraph)
 ############### Defined Functions ###############
 #################################################
 
+# Function to Generate Small World Graph
 makeSWPNetwork <- function(dim,size,nei,p){
-g <- watts.strogatz.game(dim,size,nei,p, loops = FALSE, multiple = FALSE)
+    swpGraph <- watts.strogatz.game(dim,size,nei,p, loops = FALSE, multiple = FALSE)
 
-# print(hub.score(g))
+    print(size^dim)
+    print("Vertices Count:")
+    print(vcount(swpGraph))
+    print("Edge Count:")
+    print(ecount(swpGraph))
+    print("Degree:")
+    print(degree(swpGraph))
 
-return(g)
+return(swpGraph)
 }
 
+# Function to Generate an Erdos-Renyi random graph
+makeRandNetwork <- function(dim, size, swpGraph){
+    randGraph <- erdos.renyi.game((size^dim), ecount(swpGraph), type=c("gnm"), directed = FALSE, loops = FALSE)
+    
+    print(size^dim)
+    print("Vertices Count:")
+    print(vcount(randGraph))
+    print("Edge Count:")
+    print(ecount(randGraph))
+    print("Degree:")
+    print(degree(randGraph))
+return(randGraph)
+}
 
 #################################################
 ############## Execition Code ###################
 #################################################
 
 ## Model Parameters
-dim       = 1	# Interger Constant, the demension of the starting lattice
-size      = 2 	# The size of the lattice along each dimension
+dim       = 3	# Interger Constant, the demension of the starting lattice
+size      = 4 	# The size of the lattice along each dimension
 nei       = 2   # the neighborhood within which the verticies of the lattice will be connected
 p         = .5  # the rewiring probabillity
 
 
 ## Execute
 swpGraph = makeSWPNetwork(dim,size,nei,p)
+randGraph = makeRandNetwork(dim, size, swpGraph)
 ## Plot
 png(file="SWP_plot1.png")
 plot(swpGraph)
-
-
+png(file="rand_plot1.png")
+plot(randGraph)
