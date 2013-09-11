37918522a3716b0e519472e8d39d6e665b8a0697
Ryan Himmelwright <himmelwr@cs.lafayette.edu>
2013-09-11 10:47:45 -0400

    some testing scripts to get thigs figured out

---
+++ Model/r_testing.r
@@ -0,0 +1,9 @@
+# Testing R Script
+args <- commandArgs(trailingOnly = TRUE)
+directory=(args[1])
+
+currDir = "./" + directory
+print(currDir)
+# setwd(""
+
+print(directory)
--- Model/run_models.sh
+++ Model/run_models.sh
@@ -5,6 +5,8 @@ runCount=20
 for i in $(eval echo {1..$runCount})
 do
     mkdir model$i
+    Rscript r_testing.r model$i
+    # run make_model.r with model$i as the input arguement, that causes the script to run in that directory.
 done
 
-Rscript make_model.r
+
