11f43df97b883f09848e8ec680203251e79c3c0d
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-03-24 21:46:29 -0400

    new run

--- Model/runmodels.sh
+++ Model/runmodels.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-python3 run_model.py 1 .82 >> Logs/LargeSteps_Log.txt
-python3 run_model.py 2 .82 >> Logs/LargeSteps_Log.txt
-python3 run_model.py 1 .85 >> Logs/LargeSteps_Log.txt
-python3 run_model.py 2 .85 >> Logs/LargeSteps_Log.txt
-python3 run_model.py 1 .90 >> Logs/LargeSteps_Log.txt
-python3 run_model.py 2 .90 >> Logs/LargeSteps_Log.txt
-python3 run_model.py 1 .92 >> Logs/LargeSteps_Log.txt
-python3 run_model.py 2 .92 >> Logs/LargeSteps_Log.txt
-python3 run_model.py 1 .95 >> Logs/LargeSteps_Log.txt
-python3 run_model.py 2 .95 >> Logs/LargeSteps_Log.txt
+echo $(date) >> Logs/March24_times.txt
+python3 run_model.py 1 .85 >> Logs/March24_Run_log.txt
+echo $(date) >> Logs/March24_times.txt
+python3 run_model.py 2 .85 >> Logs/March24_Run_log.txt 
+echo $(date) >> Logs/March24_times.txt
+python3 run_model.py 1 .87 >> Logs/March24_Run_log.txt 
+echo $(date) >> Logs/March24_times.txt
+python3 run_model.py 2 .87 >> Logs/March24_Run_log.txt 
+echo $(date) >> Logs/March24_times.txt
+python3 run_model.py 1 .90 >> Logs/March24_Run_log.txt 
+echo $(date) >> Logs/March24_times.txt
+python3 run_model.py 2 .90 >> Logs/March24_Run_log.txt 
+echo $(date) >> Logs/March24_times.txt
+python3 run_model.py 1 .92 >> Logs/March24_Run_log.txt 
+echo $(date) >> Logs/March24_times.txt
+python3 run_model.py 2 .92 >> Logs/March24_Run_log.txt
+echo $(date) >> Logs/March24_times.txt
