a4db3389ffbe3af3dafca77b5aafac4f6275bdf8
Ryan Himmelwright <ryan@localhost.localdomain>
2014-03-26 11:23:06 -0400

    added support to define a min and max degree between 2 and 10

--- Model/Regression_Plot.r
+++ Model/Regression_Plot.r
@@ -6,13 +6,17 @@ library(stringr)	# String Library
 # Use command arg to set input file name
 args        <- commandArgs(trailingOnly = TRUE)
 inputFile   <- args[1]
-degreeMax   <- args[2] 	# degree the regressions plots go to (up to 6)
+degreeMin   <- as.numeric(args[2])  # Degree that starts in the plot
+degreeMax   <- as.numeric(args[3]) 	# degree the regressions plots go to (up to 10)
 inputType   <- ".txt"
 
+print(degreeMax)
+
+
 plotName    <- paste(unlist(str_split(inputFile,inputType))[1],
-			   "_RegressionPlot",degreeMax,".png", sep="")
+			   "_RegressionPlot",degreeMin,"_",degreeMax,".png", sep="")
 outFileName <- paste(unlist(str_split(inputFile,inputType))[1], 
-			   "_Regressions",degreeMax,".txt", sep="")
+			   "_Regressions",degreeMin, "_", degreeMax,".txt", sep="")
 
 # Read in Data
 data <- read.csv(inputFile, header = TRUE, sep="\t")
@@ -35,63 +39,64 @@ fit10 <- lm(data$Sws2 ~ poly(data$step, 10, raw = TRUE))
 
 
 # Print the summaries of the regressions
-write("Second Degree Regression", file = outFileName, append = TRUE)
-write("------------------------", file = outFileName, append = TRUE)
-out <- capture.output(summary(fit2), file = NULL, append = FALSE)
-write(out, file = outFileName, append = TRUE)
+if(degreeMin <= 2 && degreeMax >= 2){
+	write("Second Degree Regression", file = outFileName, append = TRUE)
+	write("------------------------", file = outFileName, append = TRUE)
+	out <- capture.output(summary(fit2), file = NULL, append = FALSE)
+	write(out, file = outFileName, append = TRUE)
+}
 
-
-if(degreeMax > 2){
+if(degreeMin <= 3 && degreeMax >= 3){
 	write("Third Degree Regression", file = outFileName, append = TRUE)
 	write("------------------------", file = outFileName, append = TRUE)
 	out <- capture.output(summary(fit3), file = NULL, append = FALSE)
 	write(out, file = outFileName, append = TRUE)
 }
 
-if(degreeMax > 3){
+if(degreeMin <= 4 && degreeMax >= 4){
 	write("Fourth Degree Regression", file = outFileName, append = TRUE)
 	write("------------------------", file = outFileName, append = TRUE)
 	out <- capture.output(summary(fit4), file = NULL, append = FALSE)
 	write(out, file = outFileName, append = TRUE)
 }
 
-if(degreeMax > 4){
+if(degreeMin <= 5 && degreeMax >= 5){
 	write("Fifth Degree Regression", file = outFileName, append = TRUE)
 	write("------------------------", file = outFileName, append = TRUE)
 	out <- capture.output(summary(fit5), file = NULL, append = FALSE)
 	write(out, file = outFileName, append = TRUE)
 }
 
-if(degreeMax > 5){
+if(degreeMin <= 6 && degreeMax >= 6){
 	write("Sixth Degree Regression", file = outFileName, append = TRUE)
 	write("------------------------", file = outFileName, append = TRUE)
 	out <- capture.output(summary(fit6), file = NULL, append = FALSE)
 	write(out, file = outFileName, append = TRUE)
 }
 
-if(degreeMax > 6){
+if(degreeMin <= 7 && degreeMax >= 7){
 	write("Seventh Degree Regression", file = outFileName, append = TRUE)
 	write("------------------------", file = outFileName, append = TRUE)
 	out <- capture.output(summary(fit7), file = NULL, append = FALSE)
 	write(out, file = outFileName, append = TRUE)
 }
 
-if(degreeMax > 7){
+if(degreeMin <= 8 && degreeMax >= 8){
 	write("Eigth Degree Regression", file = outFileName, append = TRUE)
 	write("------------------------", file = outFileName, append = TRUE)
 	out <- capture.output(summary(fit8), file = NULL, append = FALSE)
 	write(out, file = outFileName, append = TRUE)
 }
 
-if(degreeMax > 8){
+if(degreeMin <= 9 && degreeMax >= 9){
 	write("Ninth Degree Regression", file = outFileName, append = TRUE)
 	write("------------------------", file = outFileName, append = TRUE)
 	out <- capture.output(summary(fit9), file = NULL, append = FALSE)
 	write(out, file = outFileName, append = TRUE)
 }
 
-if(degreeMax > 9){
-	write("Sixth Degree Regression", file = outFileName, append = TRUE)
+if(degreeMin <= 10 && degreeMax >= 10){
+	write("Tenth Degree Regression", file = outFileName, append = TRUE)
 	write("------------------------", file = outFileName, append = TRUE)
 	out <- capture.output(summary(fit10), file = NULL, append = FALSE)
 	write(out, file = outFileName, append = TRUE)
@@ -103,46 +108,50 @@ png(plotName)
 plot(data$step, data$Sws2, type="l", lwd=3)
 
 # Add second degree fit to plot
-points(data$step, predict(fit2), type="l", col="yellowhimm", lwd=2)
+if(degreeMin <= 2 && degreeMax >= 2){
+	points(data$step, predict(fit2), type="l", col="khaki", lwd=2)
+}
 
-if(degreeMax > 2){
+if(degreeMin <= 3 && degreeMax >= 3){
 	# Add third degree fit to plot
-	points(data$step, predict(fit3), type="l", col="blue", lwd=2)
+	points(data$step, predict(fit3), type="l", col="lightblue2", lwd=2)
 }
 
-if(degreeMax > 3){
+if(degreeMin <= 4 && degreeMax >= 4){
 	# Add fourth degree fit to plot
-	points(data$step, predict(fit4), type="l", col="green", lwd=2)
+	points(data$step, predict(fit4), type="l", col="gray68", lwd=2)
 }
 
-if(degreeMax > 4){
+if(degreeMin <= 5 && degreeMax >= 5){
 	# Add fifts degree fit to plot
 	points(data$step, predict(fit5), type="l", col="orange", lwd=2)
 }
 
-if(degreeMax > 5){
+if(degreeMin <= 6 && degreeMax >= 6){
 	# Add sixth degree fit to plot
-	points(data$step, predict(fit6), type="l", col="cyan", lwd=2)
+	points(data$step, predict(fit6), type="l", col="blue", lwd=2)
 }
 
-if(degreeMax > 6){
+if(degreeMin <= 7 && degreeMax >= 7){
 	# Add 7th degree fit to plot
-	points(data$step, predict(fit7), type="l", col="purple", lwd=2)
+	points(data$step, predict(fit7), type="l", col="green", lwd=2)
 }
 
-if(degreeMax > 7){
+if(degreeMin <= 8 && degreeMax >= 8){
 	# Add 8th degree fit to plot
-	points(data$step, predict(fit8), type="l", col="brown", lwd=2)
+	points(data$step, predict(fit8), type="l", col="red", lwd=2)
 }
 
-if(degreeMax > 8){
-	# Add 9th degree fit to plot
-	points(data$step, predict(fit9), type="l", col="red", lwd=2)
-}
+if(degreeMin <= 9 && degreeMax >= 9){
+ 	# Add 9th degree fit to plot
+ 	points(data$step, predict(fit9), type="l", col="cyan", lwd=2)
+ }
 
-if(degreeMax > 9){
+if(degreeMin <= 10 && degreeMax >=10){
 	# Add 10th degree fit to plot
-	points(data$step, predict(fit10), type="l", col="black", lwd=2)
+	points(data$step, predict(fit10), type="l", col="hotpink", lwd=2)
 }
 
-dev.off()
+garbage <- dev.off()
+
+print(paste("Finished processing ", inputFile))
