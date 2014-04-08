9bf0310eae88a4484b9b99de2c15f0c8d35e449a
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-04-08 08:45:17 -0400

    Added more loops for all variables

--- Model/BetweennessTest/testScript.r
+++ Model/BetweennessTest/testScript.r
@@ -1,31 +1,36 @@
 library(igraph)
 
 
-p           <- .15
 trialCounts <-  5
 count       <-  1
 
-# p Values loop
-for( i in seq(from=1, to= 4, by= 1)){
+
+# Dim value loop
+for( dim in seq(from=2, to=5, by=1)){
+
+  # size values loop
+  for( size in seq(from=2, to=7, by=1)){
+
+    # p Values loop
+    for( p in seq(from=.15, to= .45, by= .1)){
   
-  # for each trial loop
-  for( trial in seq(from = 1, to= trialCounts, by=1)){
-    g <- watts.strogatz.game(4,3,1,p)
+      # for each trial loop
+      for( trial in seq(from = 1, to= trialCounts, by=1)){
+        g <- watts.strogatz.game(4,size,1,p)
 
-    b <- betweenness(g)
+        b <- betweenness(g)
 
-    # Write to file
-    betweennessOutput = paste("betweennessData_",count,".dat", sep="")
+        # Write to file
+        betweennessOutput = paste("betweennessData_",count,".dat", sep="")
 
-    for( v in seq(from=1, to=length(b), by= 1)){
-      write(paste( v, '\t', b[v]), file = betweennessOutput, append = TRUE)
+        for( v in seq(from=1, to=length(b), by= 1)){
+          write(paste( v, '\t', b[v]), file = betweennessOutput, append = TRUE)
+        }
+        print(paste("Wrote data for sample ", count))
+        count = count + 1
 
-    }
+      }  
 
-    count = count + 1
-
+    } 
   }
-
-  p = p + .1
-
 }
