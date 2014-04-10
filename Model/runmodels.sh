f6b5a786e94f4a4d63c2331795f9ffe60b41512e
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-04-10 10:12:04 -0400

    Current trial run, testing hubs2

--- Model/runmodels.sh
+++ Model/runmodels.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-echo $(date) >> Logs/March24_times.txt
-python3 run_model.py 1 .85 >> Logs/March24_Run_log.txt
-echo $(date) >> Logs/March24_times.txt
-python3 run_model.py 2 .85 >> Logs/March24_Run_log.txt 
-echo $(date) >> Logs/March24_times.txt
-python3 run_model.py 1 .87 >> Logs/March24_Run_log.txt 
-echo $(date) >> Logs/March24_times.txt
-python3 run_model.py 2 .87 >> Logs/March24_Run_log.txt 
-echo $(date) >> Logs/March24_times.txt
-python3 run_model.py 1 .90 >> Logs/March24_Run_log.txt 
-echo $(date) >> Logs/March24_times.txt
-python3 run_model.py 2 .90 >> Logs/March24_Run_log.txt 
-echo $(date) >> Logs/March24_times.txt
-python3 run_model.py 1 .92 >> Logs/March24_Run_log.txt 
-echo $(date) >> Logs/March24_times.txt
-python3 run_model.py 2 .92 >> Logs/March24_Run_log.txt
-echo $(date) >> Logs/March24_times.txt
+python3 run_model.py 1 .85 >> Logs/April10_run_log.txt
+python3 run_model.py 2 .85 >> Logs/April10_run_log.txt
+python3 run_model.py 3 1   >> Logs/April10_run_log.txt
+python3 run_model.py 1 .87 >> Logs/April10_run_log.txt
+python3 run_model.py 2 .87 >> Logs/April10_run_log.txt
+python3 run_model.py 3 1.2 >> Logs/April10_run_log.txt
