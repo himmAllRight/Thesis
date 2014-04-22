4d6526e35027c8cb475ac11b85725ca35c2c1b6b
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-04-22 10:13:19 -0400

    new run

--- Model/runmodels.sh
+++ Model/runmodels.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-python3 run_model.py 1 .85 >> Logs/April10_run_log.txt
-python3 run_model.py 2 .85 >> Logs/April10_run_log.txt
-python3 run_model.py 3 1   >> Logs/April10_run_log.txt
-python3 run_model.py 1 .87 >> Logs/April10_run_log.txt
-python3 run_model.py 2 .87 >> Logs/April10_run_log.txt
-python3 run_model.py 3 1.2 >> Logs/April10_run_log.txt
+python3 run_model.py 2 1 >> Logs/April22_run_log.txt
+python3 run_model.py 4 1 >> Logs/April22_run_log.txt
+python3 run_model.py 4 1.2 >> Logs/April22_run_log.txt
+python3 run_model.py 4 1.5 >> Logs/April22_run_log.txt
