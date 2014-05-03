36d1c1a0bc9eee766182db4ffac4b2bba7e47f62
Ryan <ryan@ryanhimmelwright.com>
2014-05-03 13:03:40 -0400

    Wrote new connection mechanism Changed it so each sub graph gets connected to each other sub graph. Need to fix a small error in it, and then add the loop so each sub graph can get multiple connections to each other

--- Model/NewGraphGenerator.r
+++ Model/NewGraphGenerator.r
@@ -25,17 +25,38 @@ generateGraph <- function(dim, size, nei, p, n, d){
   # connect components
   gL <- vcount(g)
 
-  # Connect to each subgraph
-  for(i in seq(from=1, to=(n-1), by=1)){
-    # for each d
-    for(j in seq(from=1, to=d, by=1)){
-      x <- sample(1:gL,1)
-      y <- sample(((gL*i)+1):(gL*(i+1)),1)
+  # Connect all subgraphs to each other
+  # each subgraph
+  for(i in seq(from=1, to= n, by=1)){
+    # Link to each proceding sub-graph
+    k =
+
+    for( j in seq(from=i+1, to= n, by=1)){
+      
+
+      x <- sample( (((i-1)*gL)+1):(i*gL) ,1)
+      y <- sample( (((j-1)*gL)+1):(j*gL) ,1)
+
+      print(x)
+      print(y)
 
       G[x,y] <- TRUE
     }
 
   }
+
+
+  # Connect each subgraph to first subgraph
+  # for(i in seq(from=1, to=(n-1), by=1)){
+  #   # for each d
+  #   for(j in seq(from=1, to=d, by=1)){
+  #     x <- sample(1:gL,1)
+  #     y <- sample(((gL*i)+1):(gL*(i+1)),1)
+
+  #     G[x,y] <- TRUE
+  #   }
+
+  # }
 
 
   return(G)
@@ -53,7 +74,7 @@ p         <- as.numeric(args[4])
 
 # Group / Link parameters
 n         <- 5
-d         <- 10
+d         <- 100
 
 
 # Executable Code
