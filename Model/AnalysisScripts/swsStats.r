d73f54587fd8fedbf5d447e035619024851c845c
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-05-16 17:34:52 -0400

    Added new statistics script I'm working on

---
+++ Model/AnalysisScripts/swsStats.r
@@ -0,0 +1,12 @@
+windowSize = 500
+
+
+# Loops through each folder containing runs of a different parameter set.
+for(paramSet in paramList){
+  print(paramSet)
+  setwd(paramSet)
+
+
+
+  setwd("..")  # Move up a directory
+}
