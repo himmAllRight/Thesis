974639f9a7d8eb99221cdaaa23f971269411d5c9
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-05-16 23:09:07 -0400

    Should work now

--- Model/AnalysisScripts/swsStats.r
+++ Model/AnalysisScripts/swsStats.r
@@ -45,33 +45,50 @@ for(paramSet in paramList){
   	dataMatrix  <- MakeDataMatrix(run)
 
   	# Add new data cumulative lists
-  	hubCountsA	<- c(hubCountsA, dataMatrix[,2][1:windowSize])
-  	hubCountsB	<- c(hubCountsB, dataMatrix[,2][((totalSteps - windowSize)+1):totalSteps])
+  	hubCountsA	<- c(hubCountsA, mean(dataMatrix[,2][1:windowSize]))
+  	hubCountsB	<- c(hubCountsB, mean(dataMatrix[,2][((totalSteps - windowSize)+1):totalSteps]))
 
-  	Sws2A		<- c(Sws2A, dataMatrix[,7][1:windowSize])
-  	Sws2B		<- c(Sws2B, dataMatrix[,7][((totalSteps - windowSize)+1):totalSteps])
+  	Sws2A		<- c(Sws2A, mean(dataMatrix[,7][1:windowSize]))
+  	Sws2B		<- c(Sws2B, mean(dataMatrix[,7][((totalSteps - windowSize)+1):totalSteps]))
 
-  	pathLengthA	<- c(pathLengthA, dataMatrix[,4][1:windowSize])
-  	pathLengthB	<- c(pathLengthA, dataMatrix[,4][((totalSteps - windowSize)+1):totalSteps])
+  	pathLengthA	<- c(pathLengthA, mean(dataMatrix[,4][1:windowSize]))
+  	pathLengthB	<- c(pathLengthA, mean(dataMatrix[,4][((totalSteps - windowSize)+1):totalSteps]))
 
-  	ccA		<- c(ccA, dataMatrix[,6][1:windowSize])
-  	ccB		<- c(ccB, dataMatrix[,6][((totalSteps - windowSize)+1):totalSteps])
+  	ccA		<- c(ccA, mean(dataMatrix[,6][1:windowSize]))
+  	ccB		<- c(ccB, mean(dataMatrix[,6][((totalSteps - windowSize)+1):totalSteps]))
 
   	#print(dataMatrix[,2][1:windowSize])
   	#print(dataMatrix[,2][((totalSteps - windowSize)+1):totalSteps])
 
   }
 
-print("hubCountsA")
-#print(hubCountsA)
-print(length(hubCountsA))
 
-print("hubCountsB")
-#print(hubCountsB)
-print(length(hubCountsB))
-
+# Stats
 # Run statistics on List
-print(t.test(hubCountsA,hubCountsB))
+system('mkdir Statistics')
+
+
+# Hubs
+a <- unlist(t.test(hubCountsA,hubCountsB))
+output = paste('t = ',a[1],"\n",'df = ', a[2], '\n','p-vale = ', a[3], '\n','95 percent confidence interval: ', a[4],'  ', a[5], '\n','mean of x: ', a[6], '\n','mean of y: ', a[7])
+write(output,file="Statistics/hubCount_TTests.txt")
+
+
+# Sws2
+a <- unlist(t.test(Sws2A,Sws2B))
+output = paste('t = ',a[1],"\n",'df = ', a[2], '\n','p-vale = ', a[3], '\n','95 percent confidence interval: ', a[4],'  ', a[5], '\n','mean of x: ', a[6], '\n','mean of y: ', a[7])
+write(output,file="Statistics/Sws2_TTests.txt")
+
+
+# Path Length
+a <- unlist(t.test(pathLengthA,pathLengthB))
+output = paste('t = ',a[1],"\n",'df = ', a[2], '\n','p-vale = ', a[3], '\n','95 percent confidence interval: ', a[4],'  ', a[5], '\n','mean of x: ', a[6], '\n','mean of y: ', a[7])
+write(output,file="Statistics/pathLength_TTests.txt")
+
+# Clustering Coefficient
+a <- unlist(t.test(ccA,ccB))
+output = paste('t = ',a[1],"\n",'df = ', a[2], '\n','p-vale = ', a[3], '\n','95 percent confidence interval: ', a[4],'  ', a[5], '\n','mean of x: ', a[6], '\n','mean of y: ', a[7])
+write(output,file="Statistics/cc_TTests.txt")
 
 
   setwd("..")  # Move up a directory
