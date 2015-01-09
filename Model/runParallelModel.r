c725f22b68c8ffbc31bbad6f85157a28328fd49b
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2015-01-09 06:19:55 -0500

    Added the terrible nested loop...

--- Model/runParallelModel.r
+++ Model/runParallelModel.r
@@ -3,17 +3,33 @@ library(parallel)
 
 
 # Set param loops
-dimArray 	= c(4)
-sizeArray	= c(3)
-neiArray	= c(1)
-pArray		= c(.15, .25, .35, .45)
+dimArray 	      = c(4, 3)
+sizeArray	      = c(3, 4)
+neiArray	      = c(1)
+pArray		      = c(.15, .25, .35, .45)
 thresholdArray	= c(1, 1.2)
-nArray		= c(10, 20)
-dArray		= c(2, 5)
+nArray		      = c(10, 20)
+dArray		      = c(2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-runs 		= 30
-steps		= 5000
+runs 		        = 30
+steps		        = 5000
 
 runParamList = c()
 
-# Build param comb list
+# Build param comb list ;; A terrible nested loop...
+
+for(dim in dimArray){
+  for(size in sizeArray){
+    for(nei in neiArray){
+      for(p in pArray){
+        for(threshold in thresholdArray){
+          for(n in nArray){
+            for(d in dArray){
+             print(sum(dim, size, nei, p, threshold, n, d, runs, steps) )
+            }
+          }
+        }
+      }
+    }
+  }
+}
